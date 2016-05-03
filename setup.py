e0d4e10f219417059823bea47ac84013195da19d
Bru <id.brunoliveira@gmail.com>
2016-05-03 15:46:47 -0300

    Fix setup.py metadata.

--- setup.py
+++ setup.py
@@ -4,15 +4,15 @@ setup(
     name='ipython_sparql',
     version='0.1',
     packages=['ipython_sparql'],
-    url='',
+    url='https://github.com/baito/ipython_sparql',
     author='Bruno de Oliveira',
     author_email='ddd@dd.com.br',
     description='Jupyter/IPython Extension for Sparql queries',
     license ='MIT',
     keywords='sparql jupyter extension magic ipython',
     install_requires=[
-        'ipython == 4.2.0',
-        'SPARQLWrapper == 1.7.5',
-        'tabulate == 0.7.5'
+        'ipython',
+        'SPARQLWrapper',
+        'tabulate'
     ]
 )
